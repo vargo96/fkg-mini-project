1abfe668739a09e4dab7ed00c7cba2a388be6583
Varun Nandkumar Golani <golanivarun21@gmail.com>
2021-06-25 20:43:35 +0200

    Use of pre-trained Shallom embeddings and 6 classifier implementations.

--- fkg_mini_project.py
+++ fkg_mini_project.py
@@ -1,5 +1,14 @@
+import numpy as np
+import pandas as pd
 from owlready2 import World, Thing
 from rdflib import Namespace, Graph, Literal, URIRef
+from sklearn.model_selection import cross_val_score
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression, Perceptron
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
 
 
 class FKGMiniProject:
@@ -9,7 +18,9 @@ class FKGMiniProject:
         self.lps_path = lps_path
         self.onto = World().get_ontology(self.ontology_path).load()
         self.lps = self._read_and_parse_ttl_file(self.lps_path)
-
+        self.NS_CAR = Namespace("http://dl-learner.org/carcinogenesis#")
+        self.NS_RES = Namespace("https://lpbenchgen.org/resource/")
+        self.NS_PROP = Namespace("https://lpbenchgen.org/property/")
 
     def _read_and_parse_ttl_file(self, lps_path):
         lp_instance_list = []
@@ -29,37 +40,58 @@ class FKGMiniProject:
 
         return lp_instance_list
 
+    def get_X_y_from_lp(self, lp, emb_model_path):
+        # Load Embedding Model
+        embedding_model = pd.read_csv(emb_model_path, index_col=0)
+        # print(embedding_model)
+
+        X = np.zeros((len(lp['pos']) + len(lp['neg']), len(embedding_model.columns)))
+        y = np.zeros(len(lp['pos']) + len(lp['neg']), dtype=int)
+
+        index = 0
+
+        for pos_inst in lp['pos']:
+            X[index] = embedding_model.loc[str(self.NS_CAR) + pos_inst]
+            y[index] = 1
+            index += 1
+
+        for neg_inst in lp['neg']:
+            X[index] = embedding_model.loc[str(self.NS_CAR) + neg_inst]
+            y[index] = 0
+            index += 1
+
+        return X, y
+
+    def model_selection(self, learner, X, y, n_folds=5):
+        return cross_val_score(learner, X, y, cv=n_folds, scoring='f1').mean()
 
     def fit(self, lp):
+        # X, y = self.get_X_y_from_lp(lp, 'Shallom_entity_embeddings.csv')
+        # print(X.shape)
+        # print(y.shape)
         pass
-
 
     def score(self, lp):
         pass
 
-
     def write_result_file(self, lp_name, pos, neg, result_file="result.ttl"):
-        NS_CAR = Namespace("http://dl-learner.org/carcinogenesis#")
-        NS_RES = Namespace("https://lpbenchgen.org/resource/")
-        NS_PROP = Namespace("https://lpbenchgen.org/property/")
 
         g = Graph()
-        g.bind('carcinogenesis', NS_CAR)
-        g.bind('lpres', NS_RES)
-        g.bind('lpprop', NS_PROP)
+        g.bind('carcinogenesis', self.NS_CAR)
+        g.bind('lpres', self.NS_RES)
+        g.bind('lpprop', self.NS_PROP)
 
-        g.add((NS_RES.result_1pos, NS_PROP.belongsToLP, Literal(True)))
-        g.add((NS_RES.result_1pos, NS_PROP.pertainsTo, URIRef(NS_RES + lp_name)))
+        g.add((self.NS_RES.result_1pos, self.NS_PROP.belongsToLP, Literal(True)))
+        g.add((self.NS_RES.result_1pos, self.NS_PROP.pertainsTo, URIRef(self.NS_RES + lp_name)))
         for p in pos:
-            g.add((NS_RES.result_1pos, NS_PROP.resource, URIRef(NS_CAR + p)))
+            g.add((self.NS_RES.result_1pos, self.NS_PROP.resource, URIRef(self.NS_CAR + p)))
 
-        g.add((NS_RES.result_1neg, NS_PROP.belongsToLP, Literal(False)))
-        g.add((NS_RES.result_1neg, NS_PROP.pertainsTo, URIRef(NS_RES + lp_name)))
+        g.add((self.NS_RES.result_1neg, self.NS_PROP.belongsToLP, Literal(False)))
+        g.add((self.NS_RES.result_1neg, self.NS_PROP.pertainsTo, URIRef(self.NS_RES + lp_name)))
         for n in neg:
-            g.add((NS_RES.result_1neg, NS_PROP.resource, URIRef(NS_CAR + n)))
+            g.add((self.NS_RES.result_1neg, self.NS_PROP.resource, URIRef(self.NS_CAR + n)))
 
         g.serialize(destination=lp_name + "_"+ result_file, format='turtle')
-
 
     def print_infos_ontology(self):
         print("#"*50)
@@ -77,10 +109,117 @@ class FKGMiniProject:
         print(f"Number individuals: {len(instances)}")
         print("#"*50)
 
-
     def print_infos_lps(self):
         print("#"*50)
         print(f"Number LPs: {len(self.lps)}")
         for lp in self.lps:
-            print(f"LP ({lp['lp']}): PositiveEX - {len(lp['pos'])} | NegativeEX - {len(lp['neg'])}")
+            print(f"LP ({lp['lp']}): PositiveEX - {len(lp['pos'])} | NegativeEX - {len(lp['neg'])} | TotalEX - {len(lp['pos']) + len(lp['neg'])}")
         print("#"*50)
+
+    # Classifier Hyperparameter Optimization Start
+
+    def hp_optimize_perceptron(self, X_train, y_train):
+        param_grids = [
+            {
+                "penalty": ["l2", "l1", "elasticnet"],
+                "alpha": [0.0001, 0.001, 0.01, 0.1],
+                "max_iter": [1000, 1500, 2000, 2500, 3000]
+            }
+        ]
+
+        grid_clf = Perceptron()
+
+        grid_search = GridSearchCV(grid_clf, param_grid=param_grids, cv=5, n_jobs=-1
+                                   , scoring="f1")
+        grid_search.fit(X_train, y_train)
+
+        print("Perceptron best params:" + str(grid_search.best_params_))
+        return grid_search.best_params_
+
+    def hp_optimize_kNN(self, X_train, y_train):
+        param_grids = [
+            {
+                "n_neighbors": [2, 3, 4, 5, 6]
+            }
+        ]
+
+        grid_clf = KNeighborsClassifier()
+
+        grid_search = GridSearchCV(grid_clf, param_grid=param_grids, cv=5, n_jobs=-1
+                                   , scoring="f1")
+        grid_search.fit(X_train, y_train)
+
+        print("kNN best params:" + str(grid_search.best_params_))
+        return grid_search.best_params_
+
+    def hp_optimize_neural_network(self, X_train, y_train):
+        param_grids = [
+            {
+                "alpha": [0.0001, 0.001, 0.01, 0.1],
+                "activation": ["logistic", "tanh", "relu"],
+                "max_iter": [1500],
+                "hidden_layer_sizes": [(7, 5, 3), (7, 5, 3, 2), (7, 6, 5, 4, 3)]
+            }
+        ]
+
+        grid_clf = MLPClassifier()
+
+        grid_search = GridSearchCV(grid_clf, param_grid=param_grids, cv=5, n_jobs=-1
+                                   , scoring="f1")
+        grid_search.fit(X_train, y_train)
+
+        print("NN best params:" + str(grid_search.best_params_))
+        return grid_search.best_params_
+
+    def hp_optimize_svm(self, X_train, y_train):
+        param_grids = [
+            {
+                "C": [0.001, 0.01, 0.1, 1.0, 10.0, 100.0],
+                "gamma": ["scale"],
+                "kernel": ["linear", "poly", "rbf"]
+            }
+        ]
+
+        grid_clf = SVC()
+
+        grid_search = GridSearchCV(grid_clf, param_grid=param_grids, cv=5, n_jobs=-1
+                                   , scoring="f1")
+        grid_search.fit(X_train, y_train)
+
+        print("SVM best params:" + str(grid_search.best_params_))
+        return grid_search.best_params_
+
+    def hp_optimize_logistic_regression(self, X_train, y_train):
+        param_grids = [
+            {
+                "C": [0.001, 0.01, 0.1, 1.0, 10.0, 100.0],
+                "max_iter": [300, 400, 500]
+            }
+        ]
+
+        grid_clf = LogisticRegression()
+
+        grid_search = GridSearchCV(grid_clf, param_grid=param_grids, cv=5, n_jobs=-1
+                                   , scoring="f1")
+        grid_search.fit(X_train, y_train)
+
+        print("Logistic Regression best params:" + str(grid_search.best_params_))
+        return grid_search.best_params_
+
+    def hp_optimize_random_forest(self, X_train, y_train):
+        param_grids = [
+            {
+                "n_estimators": [25, 50, 75, 100]
+            }
+        ]
+
+        grid_clf = RandomForestClassifier()
+
+        grid_search = GridSearchCV(grid_clf, param_grid=param_grids, cv=5, n_jobs=-1
+                                   , scoring="f1")
+        grid_search.fit(X_train, y_train)
+
+        print("Random Forest best params:" + str(grid_search.best_params_))
+        return grid_search.best_params_
+
+    # Classifier Hyperparameter Optimization End
--- some_tests.py
+++ some_tests.py
@@ -1,7 +1,12 @@
 from fkg_mini_project import FKGMiniProject
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression, Perceptron
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 ontology = "carcinogenesis.owl"
-lps = "kg-mini-project-train.ttl"
+lps = "kg-mini-project-grading.ttl"
 project = FKGMiniProject(ontology, lps)
 
 # print some information about the learning problems and the ontology
@@ -13,4 +18,35 @@ lp = project.lps[0]
 lp_name = lp['lp']
 pos = lp['pos']
 neg = lp['neg']
+
+X, y = project.get_X_y_from_lp(lp, 'Shallom_entity_embeddings.csv')
+
+# Model Selection Start
+
+# Perceptron
+perceptron_best_params_dict = project.hp_optimize_perceptron(X, y)
+print("Perceptron CV Score: " + str(project.model_selection(Perceptron(**perceptron_best_params_dict), X, y)))
+
+# Neural Networks
+neural_network_best_params_dict = project.hp_optimize_neural_network(X, y)
+print("Neural Network CV Score: " + str(project.model_selection(MLPClassifier(**neural_network_best_params_dict), X, y)))
+
+# SVM
+svm_best_params_dict = project.hp_optimize_svm(X, y)
+print("SVM CV Score: " + str(project.model_selection(SVC(**svm_best_params_dict), X, y)))
+
+# Logistic Regression
+logistic_regression_best_params_dict = project.hp_optimize_logistic_regression(X, y)
+print("Logistic Regression CV Score: " + str(project.model_selection(LogisticRegression(**logistic_regression_best_params_dict), X, y)))
+
+# kNN
+kNN_best_params_dict = project.hp_optimize_kNN(X, y)
+print("kNN CV Score: " + str(project.model_selection(KNeighborsClassifier(**kNN_best_params_dict), X, y)))
+
+# Random Forest
+random_forest_params_dict = project.hp_optimize_random_forest(X, y)
+print("Random Forest CV Score: " + str(project.model_selection(RandomForestClassifier(**random_forest_params_dict), X, y)))
+
+# Model Selection End
+
 project.write_result_file(lp_name, pos, neg)
